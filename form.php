a431175bcd0a2e5ed04c4be1dc39d9b9e475161a
gerokal <gerokal@gmail.com>
2020-05-22 16:28:57 +0100

    second initial commit

--- form.php
+++ form.php
@@ -1,1 +1,17 @@
-<?php phpinfo(); ?>
+<?php
+    if (isset($_POST['submit'])){
+        $name = $_POST['name'];
+        $subject = $_POST['position'];
+        $mailFrom = $_POST['email'];
+        $phone = $_POST['phone'];
+        $message = $_POST['message'];
+
+        $mailTo = "lfl@barnesandroehamptonbandb.com";
+        $headers = "Player e-mail: ".$mailFrom.".\nPhone: ".$phone;
+        $txt = "Player name: ".$name.".\nMessage: ".$message;
+
+        mail($mailTo, $subject, $txt, $headers);
+        header("Location: mailsent.php");
+    }
+    
+?>
